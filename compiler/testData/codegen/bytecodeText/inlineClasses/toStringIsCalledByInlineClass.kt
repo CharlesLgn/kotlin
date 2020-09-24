3d6880459b92a5342df28f4df6c9bdd07c3aa609
pyos <pyos@google.com>
2020-09-24 16:21:39 +0200

    JVM_IR: check for null when converting unboxed inline classes to strings
    `C?` can be unboxed into `T?` if if wraps a reference type `T!!`, but in
    this case `null` is not a valid value to pass to `toString-impl`.
    
     #KT-42005 Fixed

--- compiler/testData/codegen/bytecodeText/inlineClasses/toStringIsCalledByInlineClass.kt
+++ compiler/testData/codegen/bytecodeText/inlineClasses/toStringIsCalledByInlineClass.kt
@@ -1,13 +1,25 @@
 // !LANGUAGE: +InlineClasses
+// Completely incorrect bytecode in some of these - see `box/inlineClasses/toStringOfUnboxedNullable.kt`
+// IGNORE_BACKEND: JVM
 
-// FILE: Z.kt
+// FILE: classes.kt
 inline class Z(val x: Int)
+inline class W(val x: Any)
 
 // FILE: test.kt
 fun testZ(z: Z) = z.toString()
+fun testZT(z: Z) = "$z"
 fun testNZ(z: Z?) = z?.toString()
+fun testNZA(z: Z?) = z.toString() // boxed
+fun testNZT(z: Z?) = "$z" // boxed
+
+fun testW(w: W) = w.toString()
+fun testWT(w: W) = "$w"
+fun testNW(w: W?) = w?.toString()
+fun testNWA(w: W?) = w.toString()
+fun testNWT(w: W?) = "$w"
 
 // @TestKt.class:
-// 0 INVOKESTATIC Z\$Erased\.toString
-// 0 INVOKESTATIC Z\-Erased\.toString
-// 2 INVOKESTATIC Z\.toString-impl \(I\)Ljava/lang/String;
+// 3 INVOKESTATIC Z\.toString-impl \(I\)Ljava/lang/String;
+// 5 INVOKESTATIC W\.toString-impl \(Ljava/lang/Object;\)Ljava/lang/String;
+// 2 INVOKESTATIC java/lang/String.valueOf
